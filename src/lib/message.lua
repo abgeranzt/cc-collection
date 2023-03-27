bf8d3e9433c372f136e15d2ae69dbf1aa1ccc75c
Marcel Engelke <marcel.engelke@posteo.net>
2023-03-28 01:07:46 +0200

    feat(message): reply and docs

--- src/lib/message.lua
+++ src/lib/message.lua
@@ -1,6 +1,14 @@
+--- @alias msg_type "res" | "cmd"
 local message_types = {
 	res = true,
 	cmd = true
+}
+
+--- @alias status "ack" | "err" | "ok"
+local status_types = {
+	ack = true,
+	err = true,
+	ok = true
 }
 
 --- @param ch number
@@ -12,7 +20,10 @@ local function worker_setup(ch, master_name, master_ch, queue, modem)
 	local message = {
 	}
 
-	--- @param msg {rec: string, snd: string, type: string, payload: {id: number, body: {cmd: string, params: table | nil} | nil}}
+	--- @alias payload {id: number, body: {cmd: string, params: table | nil} | nil, status: status | nil}
+	--- @alias msg {rec: string, snd: string, type: msg_type, payload: payload | nil }
+
+	--- @param msg msg
 	function message.validate(msg)
 		-- Drop the message if it is malformed or not intended for us.
 		if type(msg) ~= "table"
@@ -28,10 +39,19 @@ local function worker_setup(ch, master_name, master_ch, queue, modem)
 			return false
 		end
 		-- Validate command
-		if msg.payload.body then
-			if type(msg.payload.body) ~= "table"
+		if msg.type == "cmd" then
+			if not msg.payload.body
+				or type(msg.payload.body) ~= "table"
 				or not msg.payload.body.cmd
 				or type(msg.payload.body.cmd) ~= "string"
+			then
+				return false
+			end
+		end
+		-- Validate response
+		if msg.type == "res" then
+			if not msg.payload.status
+				or not status_types[msg.payload.status]
 			then
 				return false
 			end
@@ -39,13 +59,13 @@ local function worker_setup(ch, master_name, master_ch, queue, modem)
 		return true
 	end
 
-	--- @param msg {rec: string, snd: string, type: string, payload: {id: number, body: {cmd: string, params: table | nil} | nil}}
-	function message.print(msg)
+	--- @param msg msg
+	function message._print(msg)
 		print("info: [msg] " .. msg.type .. " " .. msg.payload.id)
 	end
 
-	--- @param msg {rec: string, snd: string, type: string, payload: {id: number, body: {cmd: string, params: table | nil}}}
-	function message.create_task(msg)
+	--- @param msg msg
+	function message._create_task(msg)
 		local task = {
 			reply_ch = master_ch,
 			id = msg.payload.id,
@@ -54,18 +74,41 @@ local function worker_setup(ch, master_name, master_ch, queue, modem)
 		queue.push(task)
 	end
 
+	-- Handle modem messages. This includes the following:
+	-- - decide whether to keep or drop messages
+	-- - append it to the task queue if the message is a command
+	-- - reply ("acknowledge") to messages
 	function message.listen()
 		modem.open(ch)
 		while true do
 			--- @diagnostic disable-next-line: undefined-field
 			local _e, _s, _c, _rc, msg, _d = os.pullEvent("modem_message")
 			if message.validate(msg) then
-				message.print(msg)
+				--- @cast msg msg
+				message._print(msg)
 				if msg.type == "cmd" then
-					message.create_task(msg)
+					message._create_task(msg)
+					message.reply(msg.payload.id, "ack")
 				end
 			end
 		end
+	end
+
+	--- @param id number
+	--- @param status status
+	function message.reply(id, status)
+		local msg = {
+			rec = master_name,
+			--- @diagnostic disable-next-line: undefined-field
+			snd = os.computerLabel(),
+			type = "res",
+			payload = {
+				id = id,
+				status = status
+			}
+		}
+		--- @diagnostic disable-next-line: undefined-field
+		modem.transmit(master_ch, ch, msg)
 	end
 
 	return message
