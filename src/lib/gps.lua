24feed0b73188f13e0dc74c681c1ef4ec4d1936c
Marcel Engelke <marcel.engelke@posteo.net>
2023-03-31 15:17:34 +0200

    feat(gps): master monitor

--- src/lib/gps.lua
+++ src/lib/gps.lua
@@ -1,3 +1,21 @@
+--- @param workers {[string]: {position: gps_position}}
+local function master_setup(workers, logger)
+	local function monitor()
+		while true do
+			--- @diagnostic disable-next-line: undefined-field
+			local _, msg = os.pullEvent("gps_update")
+			--- @cast msg msg
+			local pos = msg.payload.body
+			--- @diagnostic disable-next-line: cast-type-mismatch
+			--- @cast pos gps_position
+			logger.debug("updating position for worker '" .. msg.snd .. "'")
+			workers[msg.snd].position = pos
+		end
+	end
+	return {
+		monitor = monitor
+	}
+end
 
 --- @param send_gps fun(payload: { id: number, body:  msg_body_gps})
 --- @param logger logger
@@ -27,6 +45,8 @@ local function worker_setup(send_gps, logger)
 	}
 end
 
+
 return {
+	master_setup = master_setup,
 	worker_setup = worker_setup
 }
