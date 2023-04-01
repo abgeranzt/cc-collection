a5a9c171e22e8d7530064b5dfdb6d6614431a748
Marcel Engelke <marcel.engelke@posteo.net>
2023-04-01 19:01:24 +0200

    fix(tooling): prevent recursive dir creation

--- link.sh
+++ link.sh
@@ -2,12 +2,10 @@
 
 n=$(grep -m 1 'computer' < .local/ids.json | sed 's/[^0-9]//g')
 
-for f in .local*; do 
-	for i in $(seq 0 $n); do
-		mkdir -p ${f}/computer/$i
-		d=${f}/computer/$i/ccc
-		if [ ! -e $d ]; then
-			ln -s $(pwd)/src $d
-		fi
-	done
+for i in $(seq 0 $n); do
+	mkdir -p .local/computer/$i
+	d=.local/computer/$i/ccc
+	if [ ! -e $d ]; then
+		ln -s $(pwd)/src $d
+	fi
 done
