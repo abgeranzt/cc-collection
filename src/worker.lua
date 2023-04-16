c08022f1561315c55ba8fad3fbf21545a63c5531
Marcel Engelke <marcel.engelke@posteo.net>
2023-04-16 20:29:08 +0200

    feat(worker): program arguments

--- src/worker.lua
+++ src/worker.lua
@@ -1,29 +1,52 @@
 ---@diagnostic disable-next-line: unknown-cast-variable
 ---@cast peripheral peripheral
 
-local queue = require("lib.queue").queue
+---@param args table The arguments provided to the program
+local function setup(args)
+	local queue = require("lib.queue").queue
+	local modem = peripheral.find("modem")
+	if not modem then
+		print("No modem found, exiting!")
+		---@diagnostic disable-next-line: undefined-global
+		exit()
+	end
+	---@cast modem modem
 
-local modem = peripheral.find("modem")
-if not modem then
-	print("No modem found, exiting!")
-	---@diagnostic disable-next-line: undefined-global
-	exit()
+	local argparse = require("lib.argparse")
+	argparse.add_arg("log_ch", "-lc", "number", false, 9000)
+	argparse.add_arg("log_lvl", "-ll", "string", false, "info")
+	argparse.add_arg("master_ch", "-mc", "number", true)
+	argparse.add_arg("master_name", "-mn", "string", true)
+	argparse.add_arg("worker_ch", "-wc", "number", true)
+	local parsed_args, e = argparse.parse(args)
+	if not parsed_args then
+		print(e)
+		---@diagnostic disable-next-line: undefined-global
+		exit()
+	end
+	---@cast parsed_args table
+
+	local log_ch = parsed_args.log_ch
+	---@cast log_ch number
+	local log_lvl = parsed_args.log_lvl
+	---@cast log_lvl log_level
+	local master_ch = parsed_args.master_ch
+	---@cast master_ch number
+	local master_name = parsed_args.master_name
+	---@cast master_name string
+	local worker_ch = parsed_args.worker_ch
+	---@cast worker_ch number
+
+	local logger = require("lib.logger").setup(log_ch, log_lvl, nil, modem)
+	---@cast logger logger
+	local message = require("lib.message").worker_setup(worker_ch, master_name, master_ch, queue, modem, logger)
+	local gps = require("lib.gps").worker_setup(message.send_gps, logger)
+	local command = require("lib.command").miner_setup(logger)
+
+	return command, gps, message, logger, queue
 end
 
--- TODO error handling + help
-local worker_ch, master_name, master_ch = ...
-worker_ch = tonumber(worker_ch)
----@cast worker_ch number
-master_ch = tonumber(master_ch)
----@cast master_ch number
-
-local logger = require("lib.logger").setup(9000, "info", nil, modem)
--- local logger = require("lib.logger").setup(9000, "trace", "/log", modem)
----@cast logger logger
-
-local message = require("lib.message").worker_setup(worker_ch, master_name, master_ch, queue, modem, logger)
-local gps = require("lib.gps").worker_setup(message.send_gps, logger)
-local command = require("lib.command").miner_setup(logger)
+local command, gps, message, logger, queue = setup({ ... })
 
 local function work_queue()
 	while true do
