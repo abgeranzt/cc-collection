fd5036821a6fa8cd15668c2741f1e03e2daf4afe
Marcel Engelke <marcel.engelke@posteo.net>
2023-05-22 04:14:12 +0200

    feat(tooling): table_to_str function

--- src/lib/debug.lua
+++ src/lib/debug.lua
@@ -13,6 +13,22 @@ local function print_table(t, indent)
 	end
 end
 
+local function table_to_str(t)
+	local s = "{"
+	for k, v in pairs(t) do
+		if type(v) == "table" then
+			s = s .. table_to_str(v)
+		else
+			if type(v) == "boolean" then
+				v = v and "true" or "false"
+			end
+			s = s .. k .. " = " .. v .. ", "
+		end
+	end
+	return s .. "}"
+end
+
 return {
-	print_table = print_table
+	print_table = print_table,
+	table_to_str = table_to_str
 }
