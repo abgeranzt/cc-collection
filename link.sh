c41e4f594e39dd972e4e318730061b7be1319263
Marcel Engelke <marcel.engelke@posteo.net>
2023-07-01 23:12:58 +0200

    refactor(tooling): install path

--- link.sh
+++ link.sh
@@ -4,7 +4,7 @@ n=$(grep -m 1 'computer' < .local/ids.json | sed 's/[^0-9]//g')
 
 for i in $(seq 0 $n); do
 	mkdir -p .local/computer/$i
-	d=.local/computer/$i/ccc
+	d=.local/computer/$i/atref
 	if [ ! -e $d ]; then
 		ln -sf $(pwd)/src $d
 	fi
