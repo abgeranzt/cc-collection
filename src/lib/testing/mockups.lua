05f436ccff809a3424fd3b59d9ed3fcc8ca88485
Marcel Engelke <marcel.engelke@posteo.net>
2023-07-19 23:56:39 +0200

    fix(testing): proper mockup fn names

--- src/lib/testing/mockups.lua
+++ src/lib/testing/mockups.lua
@@ -1,47 +1,54 @@
 ---@diagnostic disable: undefined-global
 
 local os = {
-	pullEvent = Testing.fn("pullEvent"),
-	queueEvent = Testing.fn("queueEvent"),
-	getComputerLabel = Testing.fn("getComputerLabel"),
-	setComputerLabel = Testing.fn("setComputerLabel"),
+	pullEvent = Testing.fn("os.pullEvent"),
+	queueEvent = Testing.fn("os.queueEvent"),
+	getComputerLabel = Testing.fn("os.getComputerLabel"),
+	setComputerLabel = Testing.fn("os.setComputerLabel"),
 }
 ---@cast os os
 
 local turtle = {
-	detect = Testing.fn("detect"),
-	detectUp = Testing.fn("detectUp"),
-	detectDown = Testing.fn("detectDown"),
-	inspect = Testing.fn("inspect"),
-	inspectUp = Testing.fn("inspectUp"),
-	inspectDown = Testing.fn("inspectDown"),
-	dig = Testing.fn("dig"),
-	digUp = Testing.fn("digUp"),
-	digDown = Testing.fn("digDown"),
-	place = Testing.fn("place"),
-	placeUp = Testing.fn("placeUp"),
-	placeDown = Testing.fn("placeDown"),
-	turnLeft = Testing.fn("turnLeft"),
-	turnRight = Testing.fn("turnRight"),
-	refuel = Testing.fn("refuel"),
-	getFuelLevel = Testing.fn("getFuelLevel"),
-	select = Testing.fn("select"),
-	getSelectedSlot = Testing.fn("getSelectedSlot"),
-	getItemCount = Testing.fn("getItemCount"),
-	getItemDetail = Testing.fn("getItemDetail"),
-	transferTo = Testing.fn("transferTo"),
-	equipLeft = Testing.fn("equipLeft"),
-	equipRight = Testing.fn("equipRight"),
-	drop = Testing.fn("drop"),
-	dropUp = Testing.fn("dropUp"),
-	dropDown = Testing.fn("dropDown"),
-	suck = Testing.fn("suck"),
-	suckUp = Testing.fn("suckUp"),
-	suckDown = Testing.fn("suckDown")
+	detect = Testing.fn("turtle.detect"),
+	detectUp = Testing.fn("turtle.detectUp"),
+	detectDown = Testing.fn("turtle.detectDown"),
+	inspect = Testing.fn("turtle.inspect"),
+	inspectUp = Testing.fn("turtle.inspectUp"),
+	inspectDown = Testing.fn("turtle.inspectDown"),
+	forward = Testing.fn("turtle.forward"),
+	back = Testing.fn("turtle.back"),
+	up = Testing.fn("turtle.up"),
+	down = Testing.fn("turtle.down"),
+	dig = Testing.fn("turtle.dig"),
+	digUp = Testing.fn("turtle.digUp"),
+	digDown = Testing.fn("turtle.digDown"),
+	place = Testing.fn("turtle.place"),
+	placeUp = Testing.fn("turtle.placeUp"),
+	placeDown = Testing.fn("turtle.placeDown"),
+	turnLeft = Testing.fn("turtle.turnLeft"),
+	turnRight = Testing.fn("turtle.turnRight"),
+	refuel = Testing.fn("turtle.refuel"),
+	getFuelLevel = Testing.fn("turtle.getFuelLevel"),
+	select = Testing.fn("turtle.select"),
+	getSelectedSlot = Testing.fn("turtle.getSelectedSlot"),
+	getItemCount = Testing.fn("turtle.getItemCount"),
+	getItemDetail = Testing.fn("turtle.getItemDetail"),
+	transferTo = Testing.fn("turtle.transferTo"),
+	equipLeft = Testing.fn("turtle.equipLeft"),
+	equipRight = Testing.fn("turtle.equipRight"),
+	drop = Testing.fn("turtle.drop"),
+	dropUp = Testing.fn("turtle.dropUp"),
+	dropDown = Testing.fn("turtle.dropDown"),
+	suck = Testing.fn("turtle.suck"),
+	suckUp = Testing.fn("turtle.suckUp"),
+	suckDown = Testing.fn("turtle.suckDown")
 }
 ---@cast turtle turtle
 
+local sleep = Testing.fn("sleep")
+
 return {
 	os = os,
-	turtle = turtle
+	turtle = turtle,
+	sleep = sleep
 }
