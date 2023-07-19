25601d31e49536245d2b202b314f10259a268953
Marcel Engelke <marcel.engelke@posteo.net>
2023-07-19 05:02:24 +0200

    refactor(testing): minor

--- src/lib/testing/testing.lua
+++ src/lib/testing/testing.lua
@@ -51,30 +51,26 @@ end
 ---@param test_fn fun() The function containing all the test code to be run, including assertions
 function Testing.test(name, test_fn)
 	print("[test] " .. name)
-	Testing._tests[name] = {
-		total = 0,
-		passed = 0,
-		failed = 0
-	}
 
 	local ok, err = pcall(test_fn)
 	if not ok then
 		print_err("Caught Lua exception")
 		print(err)
 	end
-	for k, v in pairs(Testing._current_test) do
-		Testing._tests[name][k] = v
-	end
+	Testing._tests[name] = helpers.table_copy(Testing._current_test)
 
 	if not ok or Testing._tests[name].failed > 0 then
 		print_fail(term.fg.bright_red .. "Test failed!" .. term.reset)
 	end
-	local run_msg = ok and "executed successfully, "
+	local run_msg = ok
+		and "executed successfully, "
 		or term.fg.bright_red .. "failed to execute" .. term.reset .. ", "
 	local total_msg = "assertions: " .. Testing._tests[name].total .. ", "
-	local passed_msg = Testing._tests[name].passed == 0 and "passed: 0, "
+	local passed_msg = Testing._tests[name].passed == 0
+		and "passed: 0, "
 		or "passed: " .. term.fg.bright_green .. Testing._tests[name].passed .. term.reset .. ", "
-	local failed_msg = Testing._tests[name].failed == 0 and "failed: 0"
+	local failed_msg = Testing._tests[name].failed == 0
+		and "failed: 0"
 		or "failed: " .. term.fg.bright_red .. Testing._tests[name].failed .. term.reset
 	print("[test summary] " .. run_msg .. total_msg .. passed_msg .. failed_msg .. "\n")
 	Testing.reset()
@@ -88,7 +84,6 @@ end
 function Testing.assert(name, expected, actual)
 	Testing._current_test.total = Testing._current_test.total + 1
 	local trace = debug.traceback(nil, 2)
-	-- TODO does nil get counted?
 	local msg_prefix = "(Assertion) " .. name .. ": "
 	if #expected ~= #actual then
 		print_err(msg_prefix .. "Expected " .. #expected .. " values, but got " .. #actual)
